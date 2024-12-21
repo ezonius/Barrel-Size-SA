1a839abb20c3b6d04cf61761326fc12a4413e559
ThisDevDoesNotExist <thisdevdoesnotexist.fox099@passmail.net>
2024-12-21 17:46:55 +0100

    updated for Space Age and compatibility with more-barrels and pressurizedbarrels

--- data-updates.lua
+++ data-updates.lua
@@ -1,12 +1,19 @@
+---@type number
+---@diagnostic disable: assign-type-mismatch
 local TIME_FILL = settings.startup["barrel-size-fill-time"].value
+---@type integer
 local CAP = settings.startup["barrel-size-capacity"].value
 
-for _,r in pairs(data.raw.recipe) do
-	if r.subgroup=="fill-barrel" then
-		r.energy_required = TIME_FILL
-		r.ingredients[1].amount = CAP
-	elseif r.subgroup=="empty-barrel" then
-		r.energy_required = TIME_FILL
-		r.results[1].amount = CAP
-	end
+if mods["PressurizedBarrels"] then
+    require("__PressurizedBarrels__/data-final-fixes")
+end
+
+for _, recipe in pairs(data.raw.recipe) do
+    if recipe.subgroup and string.match(recipe.subgroup, "fill%-barrel") ~= nil then
+        recipe.energy_required = TIME_FILL
+        recipe.ingredients[1].amount = CAP
+    elseif recipe.subgroup and string.match(recipe.subgroup, "empty%-barrel") ~= nil then
+        recipe.energy_required = TIME_FILL
+        recipe.results[1].amount = CAP
+    end
 end
--- data.lua
+++ data.lua
@@ -1,1 +1,4 @@
-data.raw.item["empty-barrel"].stack_size=settings.startup["barrel-size-stack"].value
+---@type integer
+---@diagnostic disable-next-line: assign-type-mismatch
+local barrelSizeStack = settings.startup["barrel-size-stack"].value
+data.raw.item["barrel"].stack_size=barrelSizeStack
